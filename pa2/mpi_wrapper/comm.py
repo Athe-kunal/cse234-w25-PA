2632115bc79a0e9c19239e58edf5e88149fc42ea
Athe-kunal <athekunal@gmail.com>
2025-12-12 15:22:23 +0530

    comm implementation

--- pa2/mpi_wrapper/comm.py
+++ pa2/mpi_wrapper/comm.py
@@ -75,6 +75,43 @@ class Communicator(object):
           - For non-root processes: one send and one receive.
           - For the root process: (n-1) receives and (n-1) sends.
         """
+        assert src_array.size == dest_array.size
+        src_array_byte = src_array.itemsize * src_array.size
+        # Get_size() gives the world size
+        self.total_bytes_transferred += src_array_byte * 2 * (self.comm.Get_size() - 1)
+        # "Manual" all-reduce: reduce, then bcast
+        nprocs = self.comm.Get_size()
+        rank = self.comm.Get_rank()
+
+        # Step 1: allocate receive buffer on root, then sync
+        if rank == 0:
+            all_arrays = np.empty((nprocs, src_array.size), dtype=src_array.dtype)
+        else:
+            all_arrays = None
+        self.Barrier()
+
+        # Step 2: send all arrays to root
+        if rank == 0:
+            all_arrays[0, :] = src_array
+            for i in range(1, nprocs):
+                self.comm.Recv(all_arrays[i], source=i, tag=123)
+        else:
+            self.comm.Send(src_array, dest=0, tag=123)
+        self.Barrier()
+
+        # Step 3: reduce on root then broadcast result
+        if rank == 0:
+            if op == MPI.SUM:
+                dest_array[:] = np.sum(all_arrays, axis=0)
+            elif op == MPI.PROD:
+                dest_array[:] = np.prod(all_arrays, axis=0)
+            elif op == MPI.MIN:
+                dest_array[:] = np.min(all_arrays, axis=0)
+            elif op == MPI.MAX:
+                dest_array[:] = np.max(all_arrays, axis=0)
+            else:
+                raise ValueError("Unsupported op in myAllreduce")
+        self.comm.Bcast(dest_array, root=0)
 
     def myAlltoall(self, src_array, dest_array):
         """
@@ -91,4 +128,35 @@ class Communicator(object):
 
         The total data transferred is updated for each pairwise exchange.
         """
-        # TODO: Your code here
+        nprocs = self.comm.Get_size()
+        rank = self.comm.Get_rank()
+        seg_size = src_array.size // nprocs
+
+        # Sanity check: ensure arrays can be evenly divided among procs
+        assert src_array.size % nprocs == 0
+        assert dest_array.size % nprocs == 0
+        assert src_array.shape == dest_array.shape
+
+        # src_array is assumed to be a 1D (flattened) numpy array
+        for i in range(nprocs):
+            src_offset = i * seg_size
+            dest_offset = i * seg_size
+            if i == rank:
+                # Copy local segment directly
+                dest_array[dest_offset : dest_offset + seg_size] = src_array[
+                    src_offset : src_offset + seg_size
+                ]
+                # No bytes transferred for local copy
+            else:
+                # Prepare send and recv buffers
+                sendbuf = np.ascontiguousarray(
+                    src_array[src_offset : src_offset + seg_size]
+                )
+                recvbuf = np.empty(seg_size, dtype=src_array.dtype)
+                # Sendrecv: exchange segment with process i
+                self.comm.Sendrecv(
+                    sendbuf, dest=i, sendtag=456, recvbuf=recvbuf, source=i, recvtag=456
+                )
+                dest_array[dest_offset : dest_offset + seg_size] = recvbuf
+                # Update bytes transferred: both send and recv for each segment
+                self.total_bytes_transferred += seg_size * src_array.itemsize * 2
